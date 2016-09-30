96fb893106b3204da93856e34cec83c3e35aa1e4
ozPop <ozziepopovas@gmail.com>
2016-09-30 16:06:43 -0400

    update routes, migrate

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'notes#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
